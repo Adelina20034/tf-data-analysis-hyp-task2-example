a5c33fa08e8b62db13257b5382ce82ab4187133c
Adelina20034 <129692221+Adelina20034@users.noreply.github.com>
2023-04-13 22:53:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import ks_2samp
 
 chat_id = 5127116142 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +8,4 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-     return ks_2samp(x, y).pvalue < 0.1 # Ваш ответ, True или False
+     return ks_2samp(x, y).pvalue > 0.1 # Ваш ответ, True или False
